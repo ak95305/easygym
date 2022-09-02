0fe4fa5eb72d1c92973ca2ca2980399743c55f03
Aman <ak669212@gmail.com>
2022-09-02 17:54:34 +0530

    except last two section

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ let hamIcon = document.querySelector('.hamburger .center');
 hamIcon.addEventListener('click', ()=>{
     hamIcon.classList.toggle('active');
 });
+
+
+
+
+
+
+// Add Header Class After Scroll
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+    if (scroll >= 100) {
+        $(".nav-bar").addClass("aft-scroll");
+    }
+    else{
+        $(".nav-bar").removeClass("aft-scroll");
+    }
+});
