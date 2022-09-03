8e9da049206d509a78621a3239bbe574a81ffa05
Aman <ak669212@gmail.com>
2022-09-03 12:42:05 +0530

    navbar added

--- script.js
+++ script.js
@@ -6,8 +6,11 @@ AOS.init();
 
 // Hamburger
 let hamIcon = document.querySelector('.hamburger .center');
+let navMenu = document.querySelector('.navbar-mob');
 hamIcon.addEventListener('click', ()=>{
     hamIcon.classList.toggle('active');
+    navMenu.classList.toggle('active');
+    document.body.classList.toggle('overflow-hidden');
 });
 
 
