e1efaee7046e4bda424b818593c11620c120b95e
Aman <ak669212@gmail.com>
2022-09-06 18:04:30 +0530

    dropdown added

--- script.js
+++ script.js
@@ -39,19 +39,23 @@ $.fn.isInViewport = function () {
 
 
 $(window).on('resize scroll', function () {
-    if ($('.qr-sec').isInViewport()) {
+    // if ($('.qr-sec').isInViewport()) {
         // console.log($(window).scrollTop()-1300);
         // if($(window).scrollTop() > $('.qr-sec').offset().top){
         // console.log($('.qr-sec').offset().top);
         // }
 
-        mV = $(window).scrollTop() - $('.qr-sec').offset().top;
-
-        if (mV < 0) {
+        mV = $(window).scrollTop()*1.2 -  $('.qr-sec').offset().top;
+        
+        if (mV <= 0) {
             $('.qr-img').css('margin-left', mV);
+        } else if(mV > 0){
+            $('.qr-img').css('margin-left', 0);
         }
+        console.log($('.qr-img').css('margin-left'));
 
-    }
+
+    // }
 
 
 
@@ -82,3 +86,50 @@ $(window).on('resize scroll', function () {
 
 // AOS
 AOS.init();
+
+
+
+
+(function () {
+
+    var youtube = document.querySelectorAll(".tuts-img");
+
+    for (var i = 0; i < youtube.length; i++) {
+
+        var source = "https://img.youtube.com/vi/" + youtube[i].dataset.embed + "/sddefault.jpg";
+
+        var image = new Image();
+        image.src = source;
+        image.addEventListener("load", function () {
+            youtube[i].appendChild(image);
+        }(i));
+
+        youtube[i].addEventListener("click", function () {
+
+            var iframe = document.createElement("iframe");
+
+            iframe.setAttribute("frameborder", "0");
+            iframe.setAttribute("allowfullscreen", "");
+            iframe.setAttribute("src", "https://www.youtube.com/embed/" + this.dataset.embed + "?rel=0&showinfo=0&autoplay=1");
+
+            this.innerHTML = "";
+            this.appendChild(iframe);
+        });
+    };
+
+})();
+
+
+
+// Dropdowns Nav
+let dropDown = document.querySelectorAll('.drop-down, .drop-down a');
+let subMenu = document.querySelector('.sub-menu-holder');
+
+dropDown.forEach(item =>{
+    item.addEventListener('mouseover', ()=>{
+        subMenu.classList.add('active');
+    });
+    item.addEventListener('mouseout', ()=>{
+        subMenu.classList.remove('active');
+    });
+});
